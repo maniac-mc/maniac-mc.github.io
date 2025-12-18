3e5bed31618436f703a50b170222eb1c5a6be9b6
Simon Gravelle <simon.gravelle@live.fr>
2025-12-18 13:43:39 +0100

    Updated test energy

--- tests/integration/energy-drift/test_energy_drift.f90
+++ tests/integration/energy-drift/test_energy_drift.f90
@@ -22,7 +22,7 @@ program test_delete_and_create
     implicit none
 
     character(len=LENPATH) :: base                  ! Base path to the test input folder containing input files
-    real(real64) :: e_total, e_after_delete, e_after_create  ! Total system energies at different stages: initial, after deletion, and after creation
+    real(real64) :: e_total, e_recip_after_move     ! Total system energies at different stages: initial, after deletion, and after creation
     integer :: res_type, mol_index                  ! Residue type and molecule index used for deletion and creation moves
     logical :: passed                               ! Flag indicating whether the test passed
     real(real64) :: e_coul                          ! Coulombic contribution to the system energy
@@ -50,9 +50,6 @@ program test_delete_and_create
 
     ! ---------- Compute initial energy (module-level `energy`) ----------
     call update_system_energy(primary)
-    ! e_total = energy%total
-    ! e_coul = energy%coulomb + energy%intra_coulomb
-    ! e_long = energy%recip_coulomb + energy%ewald_self
 
     ! ---------- Identify the molecule to delete ----------
     res_type = pick_random_residue_type(thermo%is_active)
@@ -60,98 +57,55 @@ program test_delete_and_create
     call update_output_files(.false.)
 
     ! ---------- ENERGY CONSERVATION DURING TRANSLATION ----------
-    ! Create 20 molecules
+
+    ! Attempt 40 molecule creation
     thermo%chemical_potential = -0.1 ! Probability of creating the molecule is 1.0
-    do i = 1, 50
-        mol_index = primary%num%residues(res_type) + 1   ! if deletion removed it, create at new index
+    do i = 1, 40
+        status%desired_block = i
+        mol_index = primary%num%residues(res_type) + 1
         call attempt_creation_move(res_type, mol_index)
         call update_output_files(.true.)
     end do
 
-    write (*,*) "Inserted", primary%num%residues(res_type), "molecules"
+    write (*,*) "Number of residue after creation :", primary%num%residues(res_type)
 
-    ! Compute energy
-    call update_system_energy(primary)
-
-    write (*,*)
-    write (*,*) "energy before a few translation move", energy%total
-    write (*,*)
-
-    status%desired_block = 0
-
-
-    ! Attemps translation move
-    do i = 1, 5
-
+    ! Attempt 10 molecule deletion
+    thermo%chemical_potential = -10.0 ! Probability of deleting the molecule is 1.0
+    do i = 41, 50
         status%desired_block = i
+        mol_index = pick_random_molecule_index(primary%num%residues(res_type))
+        call attempt_deletion_move(res_type, mol_index)
+        call update_output_files(.true.)
+    end do
 
+    write (*,*) "Number of residue after deletion :", primary%num%residues(res_type)
+
+    ! Attemps 50 translation move
+    do i = 51, 100
+        status%desired_block = i
         mol_index = pick_random_molecule_index(primary%num%residues(res_type))
         call attempt_translation_move(res_type, mol_index)
         call update_output_files(.true.)
-        
-        e_coul = energy%coulomb + energy%intra_coulomb
-        e_long = energy%recip_coulomb + energy%ewald_self
-
-        write (*,*) "energy%recip_coulomb after a few translation move", "recip_coulomb",   energy%recip_coulomb
-
     end do
 
-    ! e_coul = energy%coulomb + energy%intra_coulomb
-    ! e_long = energy%recip_coulomb + energy%ewald_self
-    ! write (*,*) "energy after a few translation move", energy%total, e_coul, e_long
+    ! Attemps 50 rotation move
+    do i = 101, 150
+        status%desired_block = i
+        mol_index = pick_random_molecule_index(primary%num%residues(res_type))
+        call attempt_rotation_move(res_type, mol_index)
+        call update_output_files(.true.)
+    end do
 
+    e_recip_after_move = energy%recip_coulomb
+
+    ! Full recalculation of energy
     call update_system_energy(primary)
 
-    write (*,*) "recip_coulomb", energy%recip_coulomb
-
-    ! write (*,*) "energy after recalcualtion", energy%total
-
-    ! if (primary%num%residues(res_type) /= 1) then
-    !     print *, "ERROR: topology must contain exactly 1 molecule"
-    !     stop 1
-    ! end if
-
-    ! ! ---------- Delete the molecule ----------
-    ! thermo%chemical_potential = -10 ! Probability of deleting the molecule is 1.0
-    ! call attempt_deletion_move(res_type, mol_index)
-
-    ! ! Recompute energy
-    ! call compute_system_energy(primary)
-    ! e_after_delete = energy%total
-
-    ! if (abs(e_after_delete - 0.0) > tol) then
-    !     print *, "FAIL: total energy mismatch"
-    !     stop 1
-    ! end if
-
-!         mol_index = primary%num%residues(res_type) + 1   ! if deletion removed it, create at new index
-   ! thermo%chemical_potential = -0.1 ! Probability of creating the molecule is 1.0
-   ! thermo%chemical_potential = -10 ! Probability of deleting the molecule is 1.0
-
-        !call attempt_creation_move(res_type, mol_index)
-        ! call attempt_deletion_move(res_type, mol_index)
-
-!         call attempt_rotation_move(res_type, mol_index)
-
-    ! Recompute energy after creation
-    ! call compute_system_energy(primary)
-    ! e_after_create = energy%total
-    ! e_coul = energy%coulomb + energy%intra_coulomb
-    ! e_long = energy%recip_coulomb + energy%ewald_self
-
-    ! if (abs(e_coul - ref_ecoul) > tol) then
-    !     print *, "FAIL: Coulomb energy mismatch"
-    !     stop 1
-    ! end if
-
-    ! if (abs(e_long - ref_elong) > tol) then
-    !     print *, "FAIL: Long-range energy mismatch"
-    !     stop 1
-    ! end if
-
-    ! if (abs(e_after_create - ref_total) > tol) then
-    !     print *, "FAIL: total energy mismatch"
-    !     stop 1
-    ! end if
+    if (abs(energy%recip_coulomb - e_recip_after_move) > tol) then
+        write (*,*) "FAIL: energy drift during monte carlo move"
+        write (*,*) "energy%recip_coulomb", energy%recip_coulomb
+        write (*,*) "e_recip_after_move", e_recip_after_move
+        stop 1
+    end if
 
 end program test_delete_and_create
