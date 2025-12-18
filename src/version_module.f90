8e36a40847a81db88300977065573957c0e2a7d1
Simon Gravelle <simon.gravelle@live.fr>
2025-12-18 19:22:03 +0100

    Prepared non-beta release

--- src/version_module.f90
+++ src/version_module.f90
@@ -1,6 +1,6 @@
 module version_module
 
     implicit none
-    character(len=*), parameter :: version = "v0.3.5-beta"
+    character(len=*), parameter :: version = "v0.4.0"
 
 end module version_module
